82aa8d9142a34e549b1ce7692c145f74131c97b2
VikaFrontend <156185912+VikaFrontend@users.noreply.github.com>
2024-01-15 15:47:43 +0300

    routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, LoginComponent, RouterModule],
+  template: `
+  <main>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
+import {LoginComponent} from "./pages/login/login.component";
+import { RegisterComponent} from "./pages/register/register.component";
+import {MainComponent} from "./pages/main/main.component";
+
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    title: 'Login page'
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register page'
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+    title: 'Main page'
+  },
+];
+
+export default routeConfig;
 export const routes: Routes = [];
